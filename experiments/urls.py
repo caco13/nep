96dc4da4b472c0c326674ae7beeb0933441b46ee
Cassiano Reinert Novais dos Santos <crns13@gmail.com>
2017-04-13 08:22:10 -0300

    Implements versioning in experiments also by updates in studies; display experiments with study title updated.

--- experiments/urls.py
+++ experiments/urls.py
@@ -11,6 +11,9 @@ urlpatterns = [
     url(r'^experiments/(?P<experiment_id>[0-9]+)/versions/(?P<version>['
         r'0-9]+)/$', views.experiment_version_detail,
         name='experiment_version_detail'),
+    url(r'^experiments/(?P<experiment_id>[0-9]+)/versions/(?P<version>['
+        r'0-9]+)/study$', views.experiment_study_version,
+        name='experiment_study_version'),
     url(r'^experiments/(?P<experiment_id>[0-9]+)/study/$',
         views.experiment_study, name='experiment_study'),
     url(r'^participants/$', views.participants_page, name='participants'),
--- experiments/views.py
+++ experiments/views.py
@@ -29,15 +29,28 @@ def experiment_versions(request, experiment_id):
     # TODO: is that the best way?
     versions_list = list()
     versions_length = len(versions)
+
+    # We have to get study versions if experiment version changed
+    # because of a change in study.
+    study = Study.objects.filter(id=experiment.study_id).get()
+    study_versions = Version.objects.get_for_object(study)
     for i in range(0, versions_length):
-        study = Study.objects.filter(id=versions[i].field_dict[
-            'study_id']).get()
+        study_version_experiment = study_versions.filter(
+            revision=versions[i].revision.id)
+        if study_version_experiment:
+            study = study_version_experiment.get()
+        else:
+            study = Study.objects.filter(id=versions[i].field_dict[
+                'study_id']).get()
         owner = User.objects.filter(id=versions[i].field_dict[
             'owner_id']).get()
         versions_list.append({
             'title': versions[i].field_dict['title'],
             'description': versions[i].field_dict['description'],
-            'study': study.title,
+            'study': study.field_dict['title'] if study_version_experiment else
+            study.title,
+            'study_revision': study.revision.id if study_version_experiment
+            else None,
             'owner': owner.username,
             'date': versions[i].revision.date_created,
             'version': versions_length - i
@@ -52,18 +65,31 @@ def experiment_version_detail(request, experiment_id, version):
     versions = Version.objects.get_for_object(experiment)
     versions_length = len(versions)
     version = int(version)
-    study = Study.objects.filter(id=versions[versions_length -
-                                             version].field_dict[
-        'study_id']).get()
-    owner = User.objects.filter(id=versions[versions_length -
-                                            version].field_dict[
-        'owner_id']).get()
+
+    study = Study.objects.filter(
+        id=versions[versions_length - version].field_dict['study_id']
+    ).get()
+    study_versions = Version.objects.get_for_object(study)
+    study_version_experiment = study_versions.filter(
+        revision=versions[versions_length - version].revision.id)
+    if study_version_experiment:
+        study = study_version_experiment.get()
+    else:
+        study = Study.objects.filter(
+            id=versions[versions_length - version].field_dict['study_id']
+        ).get()
+
+    owner = User.objects.filter(
+        id=versions[versions_length - version].field_dict['owner_id']
+    ).get()
 
     experiment_version = {
+        'id': versions[versions_length - version].object_id,
         'title': versions[versions_length - version].field_dict['title'],
         'description': versions[versions_length - version].field_dict[
             'description'],
-        'study': study.title,
+        'study': study.field_dict['title'] if study_version_experiment else
+        study.title,
         'owner': owner.username,
         'date': versions[versions_length - version].revision.date_created,
         'version': version
@@ -84,3 +110,8 @@ def experiment_study(request, experiment_id):
     study = experiment.study
     context = {'study': study}
     return render(request, 'experiments/study_detail.html', context)
+
+
+def experiment_study_version(request, experiment_id, version):
+
+    return render(request, 'experiments/version_study_detail.html')
